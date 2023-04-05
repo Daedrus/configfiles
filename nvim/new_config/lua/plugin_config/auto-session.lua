21873a23d6f7f2b7482c587c4a576bf564c435f2
Andrei Faur <da3drus@gmail.com>
2023-04-05 21:00:43 +0200

    Change auto-session nvim plugin config
    - disable automatic session creation
    - set sessionoptions according to the plugin's README

--- nvim/new_config/lua/plugin_config/auto-session.lua
+++ nvim/new_config/lua/plugin_config/auto-session.lua
@@ -1,6 +1,7 @@
 require('auto-session').setup {
     log_level = "info",
     auto_session_suppress_dirs = { "/", "~/" },
-    auto_save_enabled = true,
-    auto_restore_enabled = true,
+    auto_session_create_enable = false,
 }
+
+vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
