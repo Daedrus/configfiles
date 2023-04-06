660ddbaffb9c326368e22f45c8e9fba52fd938fd
Andrei Faur <da3drus@gmail.com>
2023-04-06 20:45:39 +0200

    Fix name of auto-session config

--- nvim/new_config/lua/plugin_config/auto-session.lua
+++ nvim/new_config/lua/plugin_config/auto-session.lua
@@ -1,7 +1,7 @@
 require('auto-session').setup {
     log_level = "info",
     auto_session_suppress_dirs = { "/", "~/" },
-    auto_session_create_enable = false,
+    auto_session_create_enabled = false,
 }
 
 vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
