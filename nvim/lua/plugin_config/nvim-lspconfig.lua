e535f7ccd4a40c2963bb7d08590a082e52a6b8df
Andrei Faur <da3drus@gmail.com>
2023-09-16 17:37:27 +0200

    Add nix LSP (only for machines that use nix)

--- nvim/lua/plugin_config/nvim-lspconfig.lua
+++ nvim/lua/plugin_config/nvim-lspconfig.lua
@@ -75,6 +75,11 @@ local servers = {
   },
 }
 
+-- Only add the nix LSP on machines that have nix
+if vim.fn.executable('nix') == 1 then
+  servers["nil_ls"] = {}
+end
+
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
